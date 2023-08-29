4ff0061050e68c02dd6dbb1f802fb3e47864e5d6
saurabh3460 <saurabh3460@gmail.com>
2023-08-29 15:19:05 +0530

    update

--- .platform/hooks/postdeploy/01_secret_manager_set_db_and_params.sh
+++ .platform/hooks/postdeploy/01_secret_manager_set_db_and_params.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-export SECRET=$(aws secretsmanager get-secret-value --secret-id your-secret-name --region your-aws-region --query SecretString --output text)
-export PARAMS=$(aws ssm get-parameter --name your-parameter-name --region your-aws-region --output text --query Parameter.Value --with-decryption)
+export SECRET=$(aws secretsmanager get-secret-value --secret-id SECRET_NAME --region AWS_REGION --query SecretString --output text)
+export PARAMS=$(aws ssm get-parameter --name PARAMETER_NAME --region your-aws-region --output text --query Parameter.Value --with-decryption)
 
 # retrive the credentials from the SSM and ASM and add it to the .env file.
 touch .env
